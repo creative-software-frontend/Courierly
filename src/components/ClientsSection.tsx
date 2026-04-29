5ebe9ba07ac02f6a0156f1e689460b178d5de0ee
Dilouar Hossain <dilouarbd@gmail.com>
2026-04-30 00:21:40 +0600

    commit

--- src/components/ClientsSection.tsx
+++ src/components/ClientsSection.tsx
@@ -6,22 +6,6 @@ import styles from './ClientsSection.module.css';
 
 const ClientsSection = () => {
   const clients = [
-     { 
-      name: '17', 
-      logo: '/17.jpg' 
-    },
-    { 
-      name: '18', 
-      logo: '/18.jpg' 
-    },
-    { 
-      name: '19', 
-      logo: '/19.jpg' 
-    },
-    { 
-      name: '20', 
-      logo: '/20.jpg' 
-    },
     { 
       name: '1', 
       logo: '/1.jpg' 
@@ -82,7 +66,22 @@ const ClientsSection = () => {
       name: '16', 
       logo: '/16.jpg' 
     },
-   
+    { 
+      name: '17', 
+      logo: '/17.jpg' 
+    },
+    { 
+      name: '18', 
+      logo: '/18.jpg' 
+    },
+    { 
+      name: '19', 
+      logo: '/19.jpg' 
+    },
+    { 
+      name: '20', 
+      logo: '/20.jpg' 
+    },
   ];
 
   // Double the array for infinite scroll effect
